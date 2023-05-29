40c972fa64a1224ff87755e716c175863223799d
mergezy <azgardex9998@gmail.com>
2023-05-29 13:35:21 +0300

    u

--- app/src/main/java/com/example/tablo/MainActivity.java
+++ app/src/main/java/com/example/tablo/MainActivity.java
@@ -27,23 +27,48 @@ public class MainActivity extends AppCompatActivity {
         nigButn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                nigSurce.setText(""+(nSurce++));
+                nSurce++;
+                updateScore();
             }
         });
         gerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gerSurce.setText(""+(gSurce++));
+                gSurce++;
+                updateScore();
             }
         });
 
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                nigSurce.setText(""+(nSurce = 0));
-                gerSurce.setText(""+(gSurce = 0));
+                nSurce = 0;
+                gSurce = 0;
+                updateScore();
             }
         });
-
+        updateScore();
     }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("score1", nSurce);
+        outState.putInt("score2", gSurce);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null) {
+            nSurce = savedInstanceState.getInt("score1");
+            gSurce = savedInstanceState.getInt("score2");
+            updateScore();
+        }
+    }
+    private void updateScore() {
+        nigSurce.setText("" + nSurce);
+        gerSurce.setText("" + gSurce);
+    }
+
+
 }
